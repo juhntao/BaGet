481ad2c829f82bae460548c045d36ad0ad7a0a4e
juhn tao <juhntao@LIVE.COM>
2022-10-30 15:48:30 +0800

    cleanup

--- src/BaGet/Startup.cs
+++ src/BaGet/Startup.cs
@@ -4,15 +4,12 @@ using BaGet.Web;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 
 namespace BaGet
 {
@@ -80,14 +77,6 @@ namespace BaGet
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.Map("/test", (x) =>
-            {
-                x.Run(async context =>
-                {
-                    await context.Response.WriteAsync(JsonConvert.SerializeObject(context.Request.Headers));
-                });
-            });
-
             var options = Configuration.Get<BaGetOptions>();
 
             if (env.IsDevelopment())
